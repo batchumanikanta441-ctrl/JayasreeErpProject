ef619ac53cf4384ccf72f2325939a3edb12cd80c
batchumanikanta441-ctrl <batchumanikanta441@gmail.com>
2026-06-30 13:34:42 +0530

    Allow GitHub Pages origin

--- backend/app/main.py
+++ backend/app/main.py
@@ -52,7 +52,6 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "https://batchumanikanta441-ctrl.github.io",
-        "https://batchumanikanta441-ctrl.github.io/JayasreeErpProject",
     ],
     allow_credentials=True,
     allow_methods=["*"],
