a1a3afe228030d7e57802792377da927ae1fa667
batchumanikanta441-ctrl <batchumanikanta441@gmail.com>
2026-06-30 13:32:21 +0530

    Connect frontend to Render backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -51,6 +51,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://batchumanikanta441-ctrl.github.io",
+        "https://batchumanikanta441-ctrl.github.io/JayasreeErpProject",
     ],
     allow_credentials=True,
     allow_methods=["*"],
